77bd88aeeb6b3b4c007a7a001897bef90562cbbd
jsnyder159 <jw.snyder159@gmail.com>
2022-04-17 15:09:52 -0400

    fixed routes and database.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -25,8 +25,11 @@ warmStrategyCache({
 });
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
-  new CacheFirst({
+
+registerRoute(
+  ({ request }) =>
+    ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
       new CacheableResponsePlugin({
@@ -34,6 +37,6 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
       }),
     ],
   })
+);
 // T*ODO: Implement asset caching
 
-registerRoute();
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,22 +18,20 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.add({content});
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result);
 };
 
 // T*ODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transation('jate', 'readonly');
+  const jateDb = await openDB('jate', 1)
+  const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
-
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 
